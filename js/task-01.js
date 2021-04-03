52d8342e2b4e08a8d6cbd338b3029a19c02472a8
Serhii Prokopchuk <gosusosed@gmail.com>
2021-04-03 17:11:07 +0300

    делает таск 1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const listCategories = document.querySelector('#categories');
+console.log(listCategories);
+const listCategoriesCount = `В списке ${listCategories.children.length} категорий`;
+console.log(listCategoriesCount);
+
+const nameCategories = listCategories.querySelector('h2');
+console.log(nameCategories);
