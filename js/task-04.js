9b9fe0e9196529e4bc7d9dbd447740c7d330c1a6
Serhii Prokopchuk <gosusosed@gmail.com>
2021-04-04 11:57:02 +0300

    добавляет 2 решение таск 4

--- js/task-04.js
+++ js/task-04.js
@@ -17,3 +17,22 @@ btnIncrement.addEventListener('click', () => {
 console.log(valueEl);
 console.log(btnDecrement);
 console.log(btnIncrement);
+
+// Второй способ решения, не знаю какой лучше?!
+
+// const valueEl = document.querySelector('#value');
+// const btnDecrement = document.querySelector('[data-action="decrement"]');
+// const btnIncrement = document.querySelector('[data-action="increment"]');
+
+// let counterValue = 0;
+
+// const decrement = () => result(counterValue -= 1);
+// const increment = () => result(counterValue += 1);
+// const result = () => valueEl.textContent = counterValue;
+
+// btnDecrement.addEventListener('click', decrement);
+// btnIncrement.addEventListener('click', increment);
+
+// console.log(valueEl);
+// console.log(btnDecrement);
+// console.log(btnIncrement);
